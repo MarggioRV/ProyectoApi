87b66496d99d06cedfbd3e0afb489e69f6af90cf
MarggioRV <marggiorobles@hotmail.com>
2026-04-20 21:10:59 +0200

    Prosiguiendo, junto a los puertos

--- src/main/java/com/example/ProyectoWeb/ProyectoApi.java
+++ src/main/java/com/example/ProyectoWeb/ProyectoApi.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoWebApplication {
+public class ProyectoApi {
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoWebApplication.class, args);
+		SpringApplication.run(ProyectoApi.class, args);
 	}
 }
